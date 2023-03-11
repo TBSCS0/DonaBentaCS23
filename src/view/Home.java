05f45d644c9804c04a3911b6b85b8afafc8c7f9e
oscarneiva <oscarneivaeneto@gmail.com>
2023-03-11 12:03:17 -0300

    read products validated

--- src/view/Home.java
+++ src/view/Home.java
@@ -1,24 +1,37 @@
 package view;
 
+import controller.ProductController;
 import controller.UserController;
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class Home {
     private Scene scene;
     private PasswordField newPassword;
-    private Button updateBtn, deleteBtn;
-    private VBox mainBox;
+    private Button updateBtn, deleteBtn, createProductBtn, updateProductBtn, deleteProductBtn;
+    private VBox userBox, productBox;
+    private HBox buttonsBox, mainBox;
+
+    private Label userArea, productArea;
+    private TableView table;
     private UserController userController;
+    private ProductController productController;
+
 
     public Home(String activeUser){
         userController = new UserController();
+        productController = new ProductController();
         initComponents(activeUser);
     }
 
     public void initComponents(String activeUser){
+        // Create scene elements
+        userArea = new Label("Área Usuário");
+
         newPassword = new PasswordField();
         newPassword.setPromptText("Nova Senha");
 
@@ -33,8 +46,56 @@ public class Home {
             userController.delete(activeUser);
         });
 
-        mainBox = new VBox();
-        mainBox.getChildren().addAll(newPassword,updateBtn,deleteBtn);
+        productArea = new Label("Área de Produtos");
+
+        table = new TableView();
+        table.setItems(FXCollections.observableList(productController.read(activeUser)));
+
+        TableColumn productId = new TableColumn<>("ID");
+        productId.setPrefWidth(100);
+        productId.setCellValueFactory(new PropertyValueFactory<>("ID"));
+
+        TableColumn productName = new TableColumn<>("Nome");
+        productName.setPrefWidth(100);
+        productName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+
+        TableColumn productQuantity = new TableColumn<>("Quantidade");
+        productQuantity.setPrefWidth(100);
+        productQuantity.setCellValueFactory(new PropertyValueFactory<>("Quantity"));
+
+        TableColumn productValue= new TableColumn<>("Valor");
+        productValue.setPrefWidth(100);
+        productValue.setCellValueFactory(new PropertyValueFactory<>("Value"));
+
+        table.getColumns().setAll(productId,productName,productQuantity,productValue);
+
+        createProductBtn = new Button("Registrar Produto");
+        createProductBtn.setOnAction(e->{
+
+        });
+
+        updateProductBtn = new Button("Atualizar Produto");
+        updateProductBtn.setOnAction(e->{
+
+        });
+
+        deleteProductBtn = new Button("Deletar Produto");
+        deleteProductBtn.setOnAction(e->{
+
+        });
+
+        // Organize boxes area
+        userBox = new VBox();
+        userBox.getChildren().addAll(userArea,newPassword,updateBtn,deleteBtn);
+
+        buttonsBox = new HBox();
+        buttonsBox.getChildren().addAll(createProductBtn,updateProductBtn,deleteProductBtn);
+
+        productBox = new VBox();
+        productBox.getChildren().addAll(productArea,table,buttonsBox);
+
+        mainBox = new HBox();
+        mainBox.getChildren().addAll(userBox,productBox);
 
         scene = new Scene(mainBox);
     }
